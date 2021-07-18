bbaa64302aa5e37fc99f8134fd5c58cc0e67bff3
behzad nouri <behzadnouri@gmail.com>
2021-07-18 16:35:43 -0400

    releases mutex lock in write-lock as well

--- gossip/src/sync.rs
+++ gossip/src/sync.rs
@@ -1,17 +1,9 @@
-use std::{
-    ops::{Deref, DerefMut},
-    sync::{LockResult, Mutex, MutexGuard, PoisonError, RwLockReadGuard},
-};
+use std::sync::{LockResult, Mutex, PoisonError, RwLockReadGuard, RwLockWriteGuard};
 
 #[derive(Default)]
 pub struct RwLock<T> {
     inner: std::sync::RwLock<T>,
     mutex: Mutex<()>,
-}
-
-pub struct RwLockWriteGuard<'a, T: 'a> {
-    inner: std::sync::RwLockWriteGuard<'a, T>,
-    _mutex_guard: MutexGuard<'a, ()>,
 }
 
 impl<T> RwLock<T> {
@@ -34,28 +26,9 @@ impl<T> RwLock<T> {
     pub fn write(&self) -> LockResult<RwLockWriteGuard<'_, T>> {
         let mutex_lock = self.mutex.lock();
         let inner = self.inner.write();
-        let err = mutex_lock.is_err() || inner.is_err();
-        let guard = RwLockWriteGuard {
-            inner: inner.unwrap_or_else(PoisonError::into_inner),
-            _mutex_guard: mutex_lock.unwrap_or_else(PoisonError::into_inner),
-        };
-        if err {
-            Err(PoisonError::new(guard))
-        } else {
-            Ok(guard)
+        match mutex_lock {
+            Err(_) => inner.and_then(|r| Err(PoisonError::new(r))),
+            Ok(_) => inner,
         }
-    }
-}
-
-impl<'a, T> Deref for RwLockWriteGuard<'a, T> {
-    type Target = T;
-    fn deref(&self) -> &Self::Target {
-        self.inner.deref()
-    }
-}
-
-impl<'a, T> DerefMut for RwLockWriteGuard<'a, T> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.inner.deref_mut()
     }
 }
