d08c156385a4f79f95f0935755df6ccc01b2eda4
behzad nouri <behzadnouri@gmail.com>
2021-12-14 14:45:29 -0500

    forces frame-pointers and debug symbols

--- scripts/cargo-install-all.sh
+++ scripts/cargo-install-all.sh
@@ -142,7 +142,7 @@ mkdir -p "$installDir/bin"
 (
   set -x
   # shellcheck disable=SC2086 # Don't want to double quote $rust_version
-  "$cargo" $maybeRustVersion build $buildProfileArg "${binArgs[@]}"
+  RUSTFLAGS='-C force-frame-pointers=yes -g' "$cargo" $maybeRustVersion build $buildProfileArg "${binArgs[@]}"
 
   # Exclude `spl-token` binary for net.sh builds
   if [[ -z "$validatorOnly" ]]; then
@@ -151,7 +151,7 @@ mkdir -p "$installDir/bin"
 
     # the patch-related configs are needed for rust 1.69+ on Windows; see Cargo.toml
     # shellcheck disable=SC2086 # Don't want to double quote $rust_version
-    "$cargo" $maybeRustVersion \
+    RUSTFLAGS='-C force-frame-pointers=yes -g' "$cargo" $maybeRustVersion \
       --config 'patch.crates-io.ntapi.git="https://github.com/solana-labs/ntapi"' \
       --config 'patch.crates-io.ntapi.rev="97ede981a1777883ff86d142b75024b023f04fad"' \
       install --locked spl-token-cli --root "$installDir" $maybeSplTokenCliVersionArg
