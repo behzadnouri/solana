f65ca71ad1a3104eb7b696acf28f4104bcf2210f
behzad nouri <behzadnouri@gmail.com>
2025-02-26 13:51:09 -0600

    Revert "send to valid address"
    This reverts commit 7cfe55c433e01b6f154de43aa8b086b99d6c0e0b.

--- turbine/src/cluster_nodes.rs
+++ turbine/src/cluster_nodes.rs
@@ -350,14 +350,15 @@ fn get_nodes(
             }),
     )
     // Add a bunch of fake, unstaked nodes for testing.
-    .chain((0..5000).map(|_i| {
-        //50.234.242.234
+    .chain((0..5000).map(|i: u16| {
+        let addr_byte1 = (i / 256).min(255) as u8;
+        let addr_byte2 = (i % 256).min(255) as u8;
         let contact_info = ContactInfo {
             pubkey: Pubkey::new_unique(),
             wallclock: timestamp(),
             tvu_quic: None,
             tvu_udp: Some(SocketAddr::new(
-                std::net::IpAddr::V4(std::net::Ipv4Addr::new(50, 234, 242, 234)),
+                std::net::IpAddr::V4(std::net::Ipv4Addr::new(150, 150, addr_byte1, addr_byte2)),
                 8080,
             )),
         };
@@ -376,7 +377,7 @@ fn get_nodes(
     )
     .collect();
     sort_and_dedup_nodes(&mut nodes);
-    if false && should_dedup_tvu_addrs {
+    if should_dedup_tvu_addrs {
         dedup_tvu_addrs(&mut nodes);
     };
     nodes
